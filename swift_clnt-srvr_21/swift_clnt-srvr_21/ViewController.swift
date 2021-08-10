5190b9a7a8f591654fd72c15e8d1593a834e5e74
Olga Surgaeva <os@os.ru>
2021-08-10 22:23:02 +0300

    init proj

---
+++ swift_clnt-srvr_21/swift_clnt-srvr_21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_clnt-srvr_21
+//
+//  Created by Olga Surgaeva on 10.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
